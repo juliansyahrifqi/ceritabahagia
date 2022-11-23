edbcbb59bb363d94f933e741fe8434d32c0df961
Rifqi Pratama <rifqipratama9@gmail.com>
2022-11-23 15:24:47 +0700

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col h-screen justify-between">
+        <Navbar />
+
+        <div className="px-4 sm:px-6 xl:px-0 w-full">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+export default App
